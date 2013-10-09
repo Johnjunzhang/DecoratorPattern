60353ce47c160d4ea69b5157e7afdec53509b1b4
grace-liujing <graceliu@thoughtworks.com>
2013-10-10 00:32:18 +0800

    step3

--- DecoratorPattern/MilkDecorator.cs
+++ DecoratorPattern/MilkDecorator.cs
@@ -8,16 +8,7 @@
 
         public override double Cost()
         {
-            var condimentCost = 4.00;
-
-            condimentCost = condimentCost + 1.0;
-
-            if (Mocha)
-            {
-                condimentCost = condimentCost + 3.0;
-            }
-
-            return condimentCost;
+            return base.Cost() + 1.0;
         }
     }
 }
